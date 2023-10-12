aef703619c4518729e828ab99af77c104fed570b
marti <martinavalkova1999@gmail.com>
2023-10-12 22:19:25 +0300

    Initial commit

---
+++ src/main/java/com/fealrais/infoblogbe/InfoblogBeApplication.java
@@ -0,0 +1,13 @@
+package com.fealrais.infoblogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoblogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoblogBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fealrais/infoblogbe/InfoblogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fealrais.infoblogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoblogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
